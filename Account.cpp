207154ef6669e434e85824c06d8f3520ce77689a
KHANG VIEN LE/LGEVH VS FUNCTIONAL TECHNOLOGY 4(khang.le@lge.com) <khang.le@lge.com>
2021-01-13 12:13:00 +0700

    add changePassword function for user

--- Account.cpp
+++ Account.cpp
@@ -13,6 +13,9 @@
 
 using namespace std;
 
+const string ACCOUNT_LIST = "list.txt";
+const string ADMIN_LIST = "admin.txt";
+
 bool UppercaseCheck(const string toBeChecked){
 	bool found = false;
 	for (char isBeingChecking : toBeChecked){
@@ -40,7 +43,6 @@ bool LowerAndUppercaseCheck (const string toBeChecked){
 }
 
 bool SpecialCharacterCheck (const string toBeChecked){
-
 	bool found = false;
 	for (char isBeingChecking : toBeChecked){
 		if ((isBeingChecking >= 48 && isBeingChecking <= 57) ||
@@ -72,7 +74,18 @@ bool PasswordCriteriaCheck(string password){
 	return checkedOK;
 }
 
-void FileUpdate(){} // to do
+void EditInTextFile(string search, string replace, string fileName){
+	ifstream readFile(fileName);
+	ostringstream text;
+
+	text << readFile.rdbuf();
+	string content = text.str();
+	content.replace(content.find(search), string(search).length(), replace);
+	readFile.close();
+
+	ofstream out_file(fileName);
+	out_file << content;
+}
 
 class Account {
 private:
@@ -88,8 +101,7 @@ public:
 		password = newPassword;
 	}
 	bool loginCheck(){
-
-		ifstream readFile("list.txt");
+		ifstream readFile(ACCOUNT_LIST);
 		string line = "";
 		bool found = false;
 
@@ -109,7 +121,7 @@ public:
 		return found;
 	}
 	bool adminCheck(){
-		ifstream readFile("admin.txt");
+		ifstream readFile(ADMIN_LIST);
 		string adminUsername = "";
 		bool found = false;
 
@@ -124,8 +136,12 @@ public:
 };
 
 class User : public Account {
-	void changePassword(Account user){
-
+private:
+	string username;
+	string password;
+public:
+	User(string id, string pass) :username{id}, password{pass} {}
+	void changePassword(){
 		string oldPassword = "";
 		string newPassword = "";
 		string confirmNewPassword = "";
@@ -138,19 +154,20 @@ class User : public Account {
 		cout << "Confirm new password: ";
 		cin >> confirmNewPassword;
 
-		if (oldPassword != user.getPassword())
+		if (oldPassword != password)
 			cout << "Your old password is incorrect!" << endl;
 		else if (!(PasswordCriteriaCheck(newPassword)))
 			cout << "Please try again!" << endl;
 		else if (confirmNewPassword != newPassword)
 			cout << "Your confirm new password is incorrect!" << endl;
 		else{
-			user.setPassword(newPassword);
-			FileUpdate(); // update the credential database
+			password = newPassword;
+			EditInTextFile(username + " " + oldPassword,
+						   username + " " + newPassword,
+						   ACCOUNT_LIST); // update the credential database
 			cout << "Your password is changed successfully!" << endl;
 		}
 	}
-
 };
 
 int main() {
@@ -185,6 +202,8 @@ int main() {
 			cout << "Hello User!\n" <<
 					"Do you want to change your password ? <y/n>\n" <<
 					"-> Your choice: ";
+		User loggedinUser {loginUser.getUsername(), loginUser.getPassword()};
+		loggedinUser.changePassword();
 	}
 
 }
